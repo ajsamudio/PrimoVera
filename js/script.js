97abd3cbc2ea1e721213a67abcddc4efa3a94efe
ajsamudio <70294918+ajsamudio@users.noreply.github.com>
2025-06-04 15:59:42 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
+        rect.top < (window.innerHeight || document.documentElement.clientHeight) * 0.8 &&
+        rect.bottom >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
